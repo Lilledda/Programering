6c32fd8766fedfd3af17d71e0f6357015f2267ad
Lilledda <lilledda@gmail.com>
2020-09-04 13:28:11 +0200

    YOOOO

---
+++ variable.js
@@ -0,0 +1,15 @@
+let no = "ha d" 
+let yes = "YOOOOOOOOO" 
+let age = 17
+
+if (age < 17) {     
+    console.log(no) 
+} 
+
+else if (age >= 17 && age <= 25) {     
+    console.log(yes) 
+} 
+
+else {     
+    console.log("HOW") 
+}
