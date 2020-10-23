318a7ea169488588e09be5f74125aaebc66b1120
Lilledda <lilledda@gmail.com>
2020-10-23 13:10:38 +0200

    Update gissa.js

--- gissa.js
+++ gissa.js
@@ -5,3 +5,15 @@ const rl = readline.createInterface({
     output: process.stdout
   });
 
+rl.question("säg nummer mellan 1 o 10", (answer) => {
+    const nummer = Math.floor((Math.random() *10) +1)
+console.log (answer)
+
+
+if (answer == nummer) console.log("gg bror") 
+
+else if (answer < nummer) console.log ("försök igen")
+
+else if (answer > nummer) console.log ("försök igen")
+})
+
